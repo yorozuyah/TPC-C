764a9f08496f2a32efaf0e01cba9c7235ec48d06
yorozuyah <yorozuyah@gmail.com>
2016-06-01 22:36:56 +0900

    modified:   tpcc_load.sh

--- tpcc_load.sh
+++ tpcc_load.sh
@@ -1,14 +1,20 @@
 #!/bin/sh
+##################################################################
+# Please create user before execting this script.
+# mysql> grant all on ${DB}.* to ${USER}@${CLI} identified by "${PASS}"
+#
+##################################################################
 
 ### Parameter
 DBMS="mysql"
 INST_DIR="/opt/jdbcrunner-1.2/"
 SCRIPTS="${INST_DIR}/scripts/tpcc_load.js"
-WH=100
+WH=1
 DB="tpcc"
 USER="tpcc"
 PASS="tpcc"
 ENDPOINT="localhost"
+CLI="localhost"
 
 ### Setting
 export CLASSPATH="${CLASSPATH}:${INST_DIR}/jdbcrunner-1.2.jar:${INST_DIR}/lib/mysql-connector-java-5.1.18-bin.jar"
@@ -16,6 +22,8 @@ export CLASSPATH="${CLASSPATH}:${INST_DIR}/jdbcrunner-1.2.jar:${INST_DIR}/lib/my
 ### modify scripts
 sed "s/jdbc:${DBMS}:\/\/localhost:/jdbc:mysql:\/\/${ENDPOINT}:/" <  ${SCRIPTS} > ${SCRIPTS}_${DBMS}
 
+### mysql setup
+mysql -u${USER} -p${PASS} -e "create database ${DB}"
+
 ### LOAD
 java JR ${SCRIPTS}_${DBMS} -param0 ${WH}
-
